01fca70cc4799d63dbb29adbef8066ba1e1505e2
DESKTOP-2NUEPSU\danib <danibaky9@gmail.com>
2025-11-26 23:26:28 +0100

    Fix GitHub Pages structure

--- quasar.config.js
+++ quasar.config.js
@@ -46,7 +46,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+       publicPath: '/https://github.com/Baky765/ST.git/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
